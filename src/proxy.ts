15d5912880885ac4af726575b1bfe94c4aa8edae
PSBVISION <punitsinghbisht1529@gmail.com>
2026-04-21 23:17:07 +0530

    feat: integrate Clerk for authentication and add responsive mobile hook

---
+++ src/proxy.ts
@@ -0,0 +1,30 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+const isOrgSelectionRoute = createRouteMatcher(["/org-selection(.*)"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  const { userId, orgId } = await auth();
+
+  //Allow Public Routes
+  if (isPublicRoute(req)) {
+    return NextResponse.next();
+  }
+
+  //Protect Non-Public Routes
+  if (!userId) {
+    await auth.protect();
+  }
+
+  //Allow org Selection page 
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+};
