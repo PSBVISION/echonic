f592739f73a7ee6b3e7a033792ef5dca2e2a25fc
PSBVISION <punitsinghbisht1529@gmail.com>
2026-06-28 23:20:21 +0530

    feat: add dashboard layout and page components

---
+++ src/app/(dashboard)/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DashboardPage() {
+  return (
+    <div className='flex flex-1 items-center justify-center'><h1 className='text-2xl font-semibold'>DashboardPage</h1></div>
+  )
+}
+
+export default DashboardPage
